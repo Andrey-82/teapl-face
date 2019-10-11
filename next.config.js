02dce00bf8dc421e4dd0f629eccce445dc147992
Andrey <ag@infoservice.ru>
2019-10-11 11:30:05 +1000

    master Добавлено формирование карты сайта для лучшего SEO

--- next.config.js
+++ next.config.js
@@ -1,2 +1,14 @@
-const withCSS = require('@zeit/next-css');
-module.exports = withCSS();
+const withCSS = require('@zeit/next-css');console.log(__dirname);
+module.exports = withCSS({
+    exportPathMap: function () {
+            return {
+              '/': { page: '/' }
+              };
+        }
+    });
+const sitemap = require('nextjs-sitemap-generator');  
+sitemap({  
+  baseUrl: '</>',  
+  pagesDirectory: __dirname + "/pages",  
+  targetDirectory : 'static/'  
+});
